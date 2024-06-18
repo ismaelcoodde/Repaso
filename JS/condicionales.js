71ab3e6fb7172d205ca6a7b7500b93ee554d72d2
Ismael Cruz <ibcruzdeveloper@gmail.com>
2024-06-18 04:51:54 +0200

    Condicional if

---
+++ JS/condicionales.js
@@ -0,0 +1,10 @@
+'use strict'
+
+//Condicional IF Instrucciones que nos va permitir comparar algo
+var edad1 = 18;
+var edad2 = 19;
+if(edad1 > edad2){
+console.log("Edad 1 es mayor que edad 2")
+}else{
+    console.log("La edad 1 es inferior")
+}
---
+++ JS/variables.js
@@ -0,0 +1,7 @@
+//VARIABLES
+//una variable es un contenedor de información
+'use strict' // Modo estricto
+var pais = "España";
+var continente = "Europa"
+var contenido = "Ismael es de " +pais+ ', ' +continente; //Ejemplo concatenación
+console.log(contenido);
