35fa21e7c6273d90d331ee307dd808c3e8901c7f
Ismael Cruz <ibcruzdeveloper@gmail.com>
2024-06-19 03:37:32 +0200

    Switch

---
+++ JS/switch.js
@@ -0,0 +1,19 @@
+'use strict'
+
+var edad = 29;
+var imprimir = "";
+
+switch (edad) {
+
+    case 18:
+        imprimir = "Acabas de cumplir la mayoría de edad";
+        break;
+    case 29:
+        imprimir = "Eres un fracasado si aún vas por aqui";
+        break;
+    case 70:
+        imprimir = "Guadañero";
+        break;
+
+}
+console.log(imprimir);
