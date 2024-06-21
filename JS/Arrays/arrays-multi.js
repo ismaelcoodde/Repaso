d3941709700f08dda6657b21e51d2101489b6af1
Ismael Cruz <ibcruzdeveloper@gmail.com>
2024-06-21 23:30:02 +0200

    Operaciones con Arrays

--- JS/Arrays/arrays-multi.js
+++ JS/Arrays/arrays-multi.js
@@ -3,7 +3,6 @@
 /*
 Arrays Multidimensional
 */
-
 var categorias = ['Humor', 'Drama', "Acción"]
 var peliculas = ['Carate Kid', '7 Almas', 'Ali G']
 
@@ -11,3 +10,28 @@ var cine = [categorias, peliculas];
 
 console.log(cine[0][2]);
 console.log(cine[1][0]);
+
+console.log('***********************************')
+
+//Añadir  a un Array
+var juegos = ['Lineage2','Counter-Strike 1.6', 'World Of Warcraft'];
+
+var elemento = "";
+
+do{
+    elemento = prompt('Introduce tu juego favorito');
+    juegos.push(elemento);
+}while(elemento != "acabar");
+juegos.pop(); //Para que no se añada el ultimo array
+console.log(juegos)
+
+console.log('***********************************')
+
+//Borrar un Array
+var lenguajes = ['Español','Ingles', 'Frances', 'Chino'];
+var indice = lenguajes.indexOf('Frances');
+
+if (indice > -1) {
+    lenguajes.splice(indice,1)
+}
+console.log(lenguajes)
