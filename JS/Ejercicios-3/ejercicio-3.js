6b90dc707642a353f9cc096dd9d68e2269e931e6
Ismael Cruz <ibcruzdeveloper@gmail.com>
2024-06-27 23:19:08 +0200

    Ejercicio 3

--- JS/Ejercicios-3/ejercicio-3.js
+++ JS/Ejercicios-3/ejercicio-3.js
@@ -18,6 +18,8 @@
 
 window.addEventListener('load', function(){
     var formulario = document.querySelector('#formulario');
+    var box_dashed = document.querySelector('.dashed');
+    box_dashed.style.display = "none";
 
     formulario.addEventListener('submit', function(){
         console.log('Evento capturado');
@@ -26,7 +28,14 @@ window.addEventListener('load', function(){
     var apellido = document.querySelector('#apellidos').value;
     var edad = document.querySelector('#edad').value;
 
-    console.log(nombre, apellidos, edad);
+    box_dashed.style.display = "block";
 
+    var p_nombre = document.querySelector('#p_nombre span');
+    var p_apellidos = document.querySelector('#p_apellidos span');
+    var p_edad = document.querySelector('#p_edad span');
+
+        p_nombre.innerHTML = nombre;
+        p_apellidos.innerHTML = apellido;
+        p_edad.innerHTML = edad;
     })
 })
