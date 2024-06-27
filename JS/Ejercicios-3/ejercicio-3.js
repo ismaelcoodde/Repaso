7b4ce649c737ff5b9cc32b8a5c3e306782b7d166
Ismael Cruz <ibcruzdeveloper@gmail.com>
2024-06-27 19:53:56 +0200

    Ejercicio 3

---
+++ JS/Ejercicios-3/ejercicio-3.js
@@ -0,0 +1,16 @@
+'use strict'
+
+/*
+1 - Formulario campos: Nombre, Apellidos, Edad.
+2- Boton de enviar el formulario: evento submit.
+3- Mostrar datos por pantalla.
+4- Tener un boton que al darle click nos muetra los datos actuales del formulario.
+*/
+
+/** window.addEventListener('load', function(){
+    var formulario = document.querySelector('#formulario');
+
+    formulario.addEventListener('submit', function(){
+        console.log("Evento submit capturado");
+    })
+}); */
