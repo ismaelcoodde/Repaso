39d28b25d89bb45693816f1086ea69af4414a6c7
Ismael Cruz <ibcruzdeveloper@gmail.com>
2024-07-01 20:12:19 +0200

    Proyecto 1

--- JS/Fetch-Ajax/fetch.js
+++ JS/Fetch-Ajax/fetch.js
@@ -1,12 +1,32 @@
 'use strict'
 //Fetch (ajax) y peticiones a servicios / apis rest (peticiones asincronas a un servidor)
 var div_usuarios = document.querySelector('#usuarios');
-var usuarios = [];
+var div_janet = document.querySelector('#janet');
+
+
 fetch('https://reqres.in/api/users') //Esto es una promesa
-.then(data => data.json())
-.then(users => {
-    usuarios = users.data;
-    console.log(usuarios);
+getUsuario()
+    .then(data => data.json())
+    .then(users => {
+        listadoUsuario(users.data)
+
+        return getJanet();
+
+    })
+    .then(data => data.json())
+    .then(user => {
+        mostrarJanet(user)
+    });
+
+function getUsuario() {
+    return fetch('https://reqres.in/api/users/')
+}
+
+function getJanet() {
+    return fetch('https://reqres.in/api/users/2')
+}
+
+function listadoUsuario(usuarios) {
 
     usuarios.map((user, i) => {
         let nombre = document.createElement('h3');
@@ -14,10 +34,17 @@ fetch('https://reqres.in/api/users') //Esto es una promesa
         div_usuarios.appendChild(nombre);
 
         document.querySelector('.loading').style.display = 'none';
-    })
+    });
+}
+function mostrarJanet(user) {
 
-})
 
+    let nombre = document.createElement('h4');
+    nombre.innerHTML = user.first_name + " " + user.last_name;
+    div_janet.appendChild(nombre);
+
+    document.querySelector('#janet .loading').style.display = 'none';
+}
 
 
 
